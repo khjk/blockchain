0d8386410591e10034357e78470c1496b81ce09a
khjk <begywjd@naver.com>
2020-07-27 21:31:07 +0900

    first typescript

---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = "hyojung", age = 25, gender = "female";
+const sayHi = (name, age, gender) => {
+    console.log(`Hello ${name}, your age is ${age} and your gender is ${gender}`);
+};
+sayHi(name, age, gender);
+//# sourceMappingURL=index.js.map
---
+++ index.ts
@@ -0,0 +1,7 @@
+const sayHi = (name:string, age:number, gender:string) => {
+    console.log(`Hello ${name}, your age is ${age} and your gender is ${gender}`);
+}
+
+sayHi('Hyoung', 25 , 'Female');
+
+export {};
