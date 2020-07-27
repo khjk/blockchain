e28df4a9fc2ba842d681462533c2237d9f4082b8
khjk <begywjd@naver.com>
2020-07-28 00:04:17 +0900

    install crypto-js and usage

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
+import * as CryptoJS from "crypto-js";
+
 class Block {
     public index: number;
     public hash: string;
     public previousHash: string;
     public data: string;
     public timestamp: number;
+ 
+    static calculateBlockHash = (index:number, previousHash:string, timestamp:number, data:string) : string => CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+
     constructor(index:number, hash:string, previousHash:string, data:string, timestamp:number){
         this.index = index;
         this.hash = hash;
@@ -15,7 +20,13 @@ class Block {
 
 const genesisBlock:Block = new Block(0,"23423423423423","","firstblockdata",12345);
 
-let blockchain: [Block] = [genesisBlock]; //arayofBlocks
+let blockchain: Block[] = [genesisBlock]; //arayofBlocks
+
+const getBlockChain = () : Block[] => blockchain;
+
+const getLatestBlock = () : Block => blockchain[blockchain.length -1];
+
+const getNewTimeStamp = () : number => Math.round(new Date().getTime() / 1000);
 
 console.log(blockchain);
 export {};
